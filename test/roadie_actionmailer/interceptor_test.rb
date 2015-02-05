2f41f7382f0d527138fcd2dcdf515df83c1ed5dd
Asger Behncke Jacobsen <asger@8kilo.com>
2015-02-05 13:56:27 +0100

    upd

--- test/roadie_actionmailer/interceptor_test.rb
+++ test/roadie_actionmailer/interceptor_test.rb
@@ -3,9 +3,14 @@ require 'test_helper'
 module RoadieActionmailer
   describe Interceptor do
 
+    let(:fixtures_path) { File.expand_path "../fixtures", __FILE__ }
+    let(:html_header_styles) { IO.readlines(File.join(fixtures_path, 'header_styles.html')) }
+    let(:html_linked_styles) { IO.readlines(File.join(fixtures_path, 'linked_styles.html')) }
+
     before(:each) do
       ActionMailer::Base.delivery_method = :test
       ActionMailer::Base.perform_deliveries = true
+      ActionMailer::Base.default_options = { from: 'foo@bar.biz', to: 'bar@foo.biz', subject: 'Foo? Bar!', content_type: 'text/html' }
       ActionMailer::Base.deliveries.clear
     end
 
@@ -14,16 +19,14 @@ module RoadieActionmailer
     end
 
     describe '#delivering_email' do
-      it 'should inline html' do
-        ActionMailer::Base.deliver do
-          to 'receiver@example.com'
-          from 'sender@example.com'
-          subject 'Mail test'
-          content_type 'text/html'
-          body read_fixture('mail')
-        end
+      it 'should inline header stylesheets' do
+        mail = ActionMailer::Base.mail(body: html_header_styles).deliver_now
+        ActionMailer::Base.deliveries.last.body.to_s.must_match 'foooo'
+      end
 
-        ActionMailer::Base.deliveries.first.body.to_s.must_match "foo"
+      it 'should inline linked stylesheets' do
+        mail = ActionMailer::Base.mail(body: html_linked_styles).deliver_now
+        ActionMailer::Base.deliveries.last.body.to_s.must_match "foooo"
       end
     end
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,4 @@
-require 'action_view'
-require 'action_view/helpers'
+require 'action_mailer'
 require 'bundler/setup'
 require 'minitest'
 require 'minitest/autorun'
