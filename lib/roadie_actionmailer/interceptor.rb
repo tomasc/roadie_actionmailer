1692b346a2c9d658e897d197ce40dc57fede7291
Tomas Celizna <tomas.celizna@gmail.com>
2015-02-05 14:20:15 +0100

    note

--- lib/roadie_actionmailer/interceptor.rb
+++ lib/roadie_actionmailer/interceptor.rb
@@ -21,7 +21,9 @@ module RoadieActionmailer
       # TODO: add options, see https://github.com/Mange/roadie-rails/blob/master/lib/roadie/rails/options.rb 
       # (basically we only need the URL options so we can transform to absolute paths)
       # @document.url_options = …
+      # 
       # TODO: init asset providers (if asset pipeline exists, then use AssetPipelineProvider, otherwise FileSystemProvider)
+      # @document.asset_providers = …
     end
 
     def process
