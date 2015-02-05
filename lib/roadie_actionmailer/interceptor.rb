76bb1829a7a3fdb9054826d5b418891e46b55a04
Tomas Celizna <tomas.celizna@gmail.com>
2015-02-05 14:07:01 +0100

    structure

--- lib/roadie_actionmailer/interceptor.rb
+++ lib/roadie_actionmailer/interceptor.rb
@@ -2,8 +2,27 @@ require 'roadie'
 
 module RoadieActionmailer
   class Interceptor
+
     def self.delivering_email message
-      message.body = Roadie::Document.new(message.body.to_s).transform
+      new(message).transform
     end
+
+    # ---------------------------------------------------------------------
+    
+    attr_reader :document
+    attr_reader :message
+
+    def initialize message
+      @message = message
+      @document = Roadie::Document.new(@message)
+    end
+
+    def process
+      # if the email is (i think) single part and is html
+      #   process it directly
+      # if it is multipart
+      #   alter only the html part (or replace it with new one)
+    end
+
   end
 end
